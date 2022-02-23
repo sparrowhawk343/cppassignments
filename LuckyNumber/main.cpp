6521777b2ba94b673330b0a01b78f5a13822acf7
Victor Järnberg <victor.jarnberg@futuregames.nu>
2022-02-23 17:11:39 +0100

    working on lucky number exercise

--- LuckyNumber/main.cpp
+++ LuckyNumber/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int input;
 
+void IsLucky(int number);
+
 int main()
 {
 	cout << "Enter an integer value to check for luck: " << endl;
 	cin >> input;
+	IsLucky(input);
+}
+
+
+void IsLucky(int number) {
+	string conversion = to_string(number);
+	vector<char> digitsAsLetters;
+
+	for (char letter : conversion) {
+		digitsAsLetters.push_back(letter);
+	}
+
 
 }
